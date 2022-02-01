de775aa083cd777648ff108b9ce4189b1fceaf8d
Caswell Orr <caswell.orr@gmail.com>
2022-02-01 13:25:03 -0600

    refactoring auth

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,33 +1,31 @@
-const { User, Book } = require('../models');
+const { User, Book } = require("../models");
 
-const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    
     // ========= GET_ME =======
 
-    me: async () => {
-      return User.findOne({ username, _id }).populate('books');
+    me: async (parent, args, { user }) => {
+      return User.findById(user._id).populate("books");
     },
   },
 
   Mutation: {
-
     // =========== LOGIN_USER =============
 
     loginUser: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const token = signToken(user);
@@ -39,33 +37,54 @@ const resolvers = {
 
     addUser: async (parent, args) => {
       const user = await User.create(args);
-      return user;
+
+      if (!user) {
+        console.log("Something is wrong!");
+      }
+
+      const token = signToken(user);
+
+      return { user, token };
     },
 
     // =========== SAVE_BOOK =============
 
-    saveBook: async (parent, { user }) => {
+    saveBook: async (parent, { user }, context) => {
 
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: user._id },
-        { new: true }
-      );
+      console.log({ user });
 
-      return updatedUser;
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: context } },
+          { new: true, runValidators: true }
+        );
 
+        return updatedUser;
+
+      } catch (error) {
+        console.log(error);
+      }
     },
 
     // =========== REMOVE_BOOK =============
 
-    removeBook: async (parent, { user }) => {
+    removeBook: async (parent, { user }, context) => {
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $pull: { savedBooks: { bookId: params.bookId } } },
+          { new: true }
+        );
 
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: user._id },
-        {new: true}
-      );
-
-      return updatedUser;
-
+        return updatedUser;
+        
+      } catch (error) {
+        if (!updatedUser) {
+          console.log("Couldn't find user with this id!");
+          console.log(error);
+        }
+      }
     },
   },
 };
